91406fedeaedb0688cd1c8029ce7a666815d1387
higo.souza <higo.souza@vitru.com.br>
2024-07-08 22:28:35 -0300

    exercício 3
    exercício 3

---
+++ 3_tipos_de_dados/2_exercicio_3/index.php
@@ -0,0 +1,13 @@
+<?php
+    //Imprima três números inteiros;
+
+    echo 5;
+    echo "<br>";
+    echo 50;
+    echo "<br>";
+    
+    //php não separa a casa de milhar
+    echo 5000;
+    echo "<br>";
+    
+?>
