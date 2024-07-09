317f4278ca1257097e78441fbe4afc901c82082c
higo.souza <higo.souza@vitru.com.br>
2024-07-08 21:06:58 -0300

    instrução de código
    instrução de código

---
+++ 2_conceitos_basicos/2_instrucao_de_codigo/index.php
@@ -0,0 +1,16 @@
+<?php
+    echo "Uma linha <br>";
+    echo "Outra linha <br>";
+
+    //instrutura de controle, if
+    //intrução if
+    //estrutura de comparação >
+    if(5 > 2){//expressão verdadeira
+        //limita o bloco do if, abrindo e fechando as chaves
+        echo "Dentro do if";//consegue imprimir, pois a expressão é verdadeira
+    }
+
+    /*
+    uma linha, ponto e vírgula
+    multi-linha, instrução com chaves
+    */
