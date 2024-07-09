8027e573868992b128e50b2cecaccbffb0ac53a4
higo.souza <higo.souza@vitru.com.br>
2024-07-08 21:32:32 -0300

    comentários em php
    comentários em php

---
+++ 2_conceitos_basicos/4_comentarios/index.php
@@ -0,0 +1,12 @@
+<?php
+    //Este é um comentário, vai ser ignorado pelo interpretador
+    echo "Teste comentário"; //Isso aqui imprime algo na tela
+
+    #Outro tipo de comentário
+    
+    /*
+    Comentário
+    multi linha
+    */
+
+    //Comentário serve para compartilhar informações sobre o projeto
