5943e5f44ee9b43279b7e9094bad2f2e3a8602e3
higo.souza <higo.souza@vitru.com.br>
2024-07-08 21:50:58 -0300

    palavras reservadas
    palavras reservadas

---
+++ 2_conceitos_basicos/6_palavras_reservadas/index.php
@@ -0,0 +1,13 @@
+<?php
+    //função
+    function echo(){
+        return true;
+    }
+
+    function while(){
+        return true;
+    }
+    /*
+    erro de sintaxe, a palvra echo esta inesperado no código
+    mesmo erro com o while, pois são palavras reservadas da liguagem
+    */
