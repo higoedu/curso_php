96c1ed8b100465079564a10c2f001b2c6ef97970
higo.souza <higo.souza@vitru.com.br>
2024-07-11 19:56:35 -0300

    exercício 5
    exercício 5

---
+++ 3_tipos_de_dados/9_exercicio_5/index.php
@@ -0,0 +1,10 @@
+<?php
+    //Imprima textos com aspas duplas e também com aspas simples;
+
+    echo 'Testando aspas simples <br>';
+    echo "Teste de aspas duplas <br>";
+    
+    $nome = "Higo";
+    $idade = 43;
+
+    echo "Olá, eu sou o $nome e tenho $idade anos";//string com interpolação de variável, string dinâmica
