f632c9023d50d59aa3f3d2f2e8d7e55740724d5c
higo.souza <higo.souza@vitru.com.br>
2024-07-11 19:45:19 -0300

    float
    float

---
+++ 3_tipos_de_dados/4_float/index.php
@@ -0,0 +1,14 @@
+<?php
+    $a = 1.12;//float adicionado a uma variável
+
+    echo $a;//impressão dele na página
+    echo "<br>";//quebra a linha
+    echo 1,123;//entende como um número só, não faz a semparação
+
+    //caso queira representar um float, temos que colocar um .
+
+    echo "<br>";
+    echo 12.5 + 1.3278;//operações aritméticas
+
+    echo "<br>";
+    echo 12 + 1.3278;//operações com tipos distintos, int com float
