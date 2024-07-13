3f03089c4ac14755ba4f8085e4848edddf47ecee
higo.souza <higo.souza@vitru.com.br>
2024-07-12 23:31:46 -0300

    exercício 9
    exercício 9

---
+++ 4_variaveis/3_exercicio_9/index.php
@@ -0,0 +1,14 @@
+<?php
+    /*
+    crie duas variáveis com números;
+    cria uma terceira com a soma destes dois números;
+    lembrando: a soma pode ser feita com o símbolo +;
+    ex: 2 + 4
+    */
+
+    $a = 11;//int
+    $b = 99.324;//float
+
+    $c = $a + $b;
+
+    echo $c;
