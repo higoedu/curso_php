48d4454e2ec3d69137df86ae668aa0890c2eca4f
higo.souza <higo.souza@vitru.com.br>
2024-07-12 23:33:12 -0300

    variável de variável
    variável de variável

---
+++ 4_variaveis/4_var_de_var/index.php
@@ -0,0 +1,10 @@
+<?php
+    $x = "nome";//valor de nome
+
+    echo "$x <br>";
+
+    $$x = "Higo";//var com o nome de x (nome) "que é nome", com valor de Higo "que é o quê eu atrinuí para ela"
+    //replica o $ 2 vezes, vai ganhar a propriedade de utilizar esse valor com nome de variável
+    //e posso atribuir um novo valor, para ser um valor dessa minha nova variável
+
+    echo "$nome <br>";
